2017f038297efe5b4d6d9952e32986b7c58bd1aa
Gautam Simplia <gautam_simplia@outlook.com>
2025-12-04 22:57:31 +0530

    Home page jobs population done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup>
-import Navbar from '@/components/Navbar.vue'
-import Hero from './components/Hero.vue';
-import Homecards from './components/Homecards.vue';
-import JobListings from './components/JobListings.vue';
-
+    import Navbar from '@/components/Navbar.vue'
+    import Hero from './components/Hero.vue';
+    import Homecards from './components/Homecards.vue';
+    import JobListings from './components/JobListings.vue';
 </script>
 
 <template>
-<Navbar />
-<Hero />
-<Homecards />
-<JobListings />
-</template>
+    <Navbar />
+    <Hero />
+    <Homecards />
+    <JobListings :limit="3" :showButton="true" />
 
+</template>
