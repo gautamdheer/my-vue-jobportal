0a6ccb33a058ed7eca5eef5b8bf788f289f83da8
Gautam Simplia <gautam_simplia@outlook.com>
2025-12-09 14:11:34 +0530

    Delete functionality done.

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -6,18 +6,35 @@
     } from 'vue';
     import {
         useRoute,
-        RouterLink
+        RouterLink,
+        useRouter
     } from 'vue-router';
     import axios from 'axios';
+import { useToast } from 'vue-toastification';
 
     const route = useRoute();
-
+    const router = useRouter();
+    const toast = useToast();
     const jobId = route.params.id;
 
     const state = reactive({
         job: {},
         isLoading: true
     });
+
+    // delete job
+    const deleteJob = async() =>{
+        try {
+            const confirm = window.confirm("Are are you sure to delete the job");
+           if(confirm){
+            await axios.delete(`/api/jobs/${jobId}`);
+            toast.success('Delete Job Successfully');
+            router.push('/jobs');
+           }
+        } catch (error) {
+            toast.error('There is problem while deleting the job',error)
+        }
+    }
 
     onMounted(async () => {
         try {
@@ -102,7 +119,7 @@
                         <RouterLink :to="`/jobs/edit/${state.job.id}`"
                             class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">Edit
                             Job</RouterLink>
-                        <button
+                        <button @click="deleteJob"
                             class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                             Delete Job
                         </button>
